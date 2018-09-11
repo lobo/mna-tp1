d1fd410f8dfc59a55634616ac72a673e88b1b5bd
foviedoITBA <foviedo@itba.edu.ar>
2018-09-11 00:26:38 -0300

    Add classifier

--- image.py
+++ image.py
@@ -6,10 +6,13 @@ VERTICAL_SIZE = 112
 HORIZONTAL_SIZE = 92
 NORMALIZE_FACTOR = 255.0
 
+IMAGES_PER_DIRECTORY = 10
+
 def load_images(directory):
     """Load images and return them as a byte array representing the grayscale values."""
     subdirectories = [f for f in listdir(directory)]
     grayscale_images = [[],[]]
+    categories = list()
 
     for face in subdirectories:
         images_paths = [(directory + "/" +  face + "/" +  f) for f in 
@@ -18,7 +21,7 @@ def load_images(directory):
             grayscale_image = to_grayscale(image_path)
             grayscale_images[0].append(grayscale_image)
             grayscale_images[1].append(grayscale_image)
-    return np.asarray(grayscale_images[0])
+    return np.asarray(grayscale_images[0]), subdirectories
 
 def to_grayscale(image_path):
     return list(Image.open(image_path).convert('L').tobytes())
--- main.py
+++ main.py
@@ -3,17 +3,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import image
 import eig
+from sklearn import svm
+
+USED_EIGEN_FACES = 10
 
 parser = argparse.ArgumentParser()
 parser.add_argument("directory", help="Complete path with sub-directories which contain images.")
 args = parser.parse_args()
 
-grayscaled_images = image.load_images(args.directory)
+print("Loading images...")
+grayscaled_images, categories = image.load_images(args.directory)
 
 # Normalizing
 grayscaled_images = grayscaled_images / image.NORMALIZE_FACTOR
 
-# Substract the mean face
+print("Subtracting mean face...")
+# Subtract the mean face
 mean_face =  np.mean(grayscaled_images, 0)
 grayscaled_images -= mean_face
 
@@ -32,21 +37,43 @@ grayscaled_images -= mean_face
 # where u is a column of U, σ is the corresponding singular value and v is the corresponding column of V
 left_covariance_matrix = grayscaled_images.dot(grayscaled_images.transpose())
 
+print("Computing eigenvalues...")
 eigen_values = eig.sorted_eigen_values(left_covariance_matrix)
 first_left_singular_vector = eig.inverse_iteration(left_covariance_matrix, eigen_values[0])
 eigen_face = grayscaled_images.transpose().dot(first_left_singular_vector)/np.sqrt(eigen_values[0])
 
 # First eigenface according to us
-eigen1 = (np.reshape(eigen_face,[image.VERTICAL_SIZE, image.HORIZONTAL_SIZE]))*image.NORMALIZE_FACTOR
-fig, axes = plt.subplots(1,1)
-axes.imshow(eigen1,cmap='gray')
-fig.suptitle('First eigenface according to us')
-plt.show()
+#eigen1 = (np.reshape(eigen_face,[image.VERTICAL_SIZE, image.HORIZONTAL_SIZE]))*image.NORMALIZE_FACTOR
+#fig, axes = plt.subplots(1,1)
+#axes.imshow(eigen1,cmap='gray')
+#fig.suptitle('First eigenface according to us')
+#plt.show()
 
 # First eigenface according to pfierens
-U,S,V = np.linalg.svd(grayscaled_images,full_matrices = False)
-eigen1 = (np.reshape(V[0,:],[image.VERTICAL_SIZE,image.HORIZONTAL_SIZE]))*image.NORMALIZE_FACTOR
-fig, axes = plt.subplots(1,1)
-axes.imshow(eigen1,cmap='gray')
-fig.suptitle('First eigenface according to pfierens')
-plt.show()
+#U,S,V = np.linalg.svd(grayscaled_images,full_matrices = False)
+#eigen1 = (np.reshape(V[0,:],[image.VERTICAL_SIZE,image.HORIZONTAL_SIZE]))*image.NORMALIZE_FACTOR
+#fig, axes = plt.subplots(1,1)
+#axes.imshow(eigen1,cmap='gray')
+#fig.suptitle('First eigenface according to pfierens')
+#plt.show()
+
+# Compute the first eigenfaces
+eigen_faces = list()
+print("Computing eigenfaces...")
+for i in range(USED_EIGEN_FACES):
+	left_singular_vector = eig.inverse_iteration(left_covariance_matrix, eigen_values[i])
+	eigen_face = grayscaled_images.transpose().dot(left_singular_vector)/np.sqrt(eigen_values[0])
+	eigen_face = [item for sublist in eigen_face for item in sublist]	# The above computation yields the eigenface as a list of one-dimentional lists, so we flatten it
+	eigen_faces.append(eigen_face)
+eigen_faces = np.asarray(eigen_faces)
+
+# Project
+print("Projecting images...")
+projected_images = np.dot(grayscaled_images, eigen_faces.transpose())
+
+# Classify
+print("Classifying...")
+classifier = svm.LinearSVC()
+image_classes = [category for category in categories for _ in range(USED_EIGEN_FACES)]
+classifier.fit(projected_images, image_classes)
+
